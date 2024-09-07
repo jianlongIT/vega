59491e3a78acc161c5e2f35829b3c70a32101045
jianlongIT <328852120@qq.com>
2024-09-07 19:48:35 +0800

    20240907 commit

--- service/user_service.py
+++ service/user_service.py
@@ -6,6 +6,14 @@ from db.user_dao import UserDao
 class UserService(object):
     __user_dao = UserDao()
 
+    def login(self, username, password):
+        result = self.__user_dao.login(username, password)
+        return result
+
+    def search_user_role(self, username):
+        result = self.__user_dao.search_user_role(username)
+        return result
+
 
 if __name__ == '__main__':
     pass
